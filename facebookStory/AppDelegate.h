10f1bbb929ce40f8bdb8a9d7c34f92be6d83def0
ivipul <ivipul@ivipul-macbookpro.roam.corp.google.com>
2014-06-23 08:59:01 -0700

    Initial Commit

---
+++ facebookStory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  facebookStory
+//
+//  Created by Vipul Thakur on 6/23/14.
+//  Copyright (c) 2014 ivipul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
